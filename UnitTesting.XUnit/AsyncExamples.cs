918ec9ee11ac7317f3dd31cfad73f6db1a457949
Kuzmin Nick <kuzmin.nick@rambler.ru>
2020-12-11 23:46:50 +0300

    XUnit samples

---
+++ UnitTesting.XUnit/AsyncExamples.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+public class AsyncExamples
+{
+    /// <summary>
+    /// Assert.ThrowsAsync
+    /// Record.ExceptionAsync
+    /// </summary>
+
+    [Fact]
+    public async void CodeThrowsAsync()
+    {
+        Func<Task> testCode = () => Task.Factory.StartNew(ThrowingNotImplementedExceptionMethod);
+
+        var ex = await Assert.ThrowsAsync<NotImplementedException>(testCode);
+
+        Assert.IsType<NotImplementedException>(ex);
+    }
+
+    [Fact]
+    public async void CodeThrowsAsync2()
+    {
+        Func<Task> testCode = () => Task.Factory.StartNew(ThrowingExceptionMethod);
+
+        var ex = await Assert.ThrowsAnyAsync<Exception>(testCode);
+
+        Assert.IsNotType<NotImplementedException>(ex);
+    }
+
+    [Fact]
+    public async void CodeThrowsAsync3()
+    {
+        Func<Task> testCode = () => Task.Factory.StartNew(ThrowingExceptionMethod);
+
+        var ex = await Assert.ThrowsAsync(typeof(Exception), testCode);
+
+        Assert.IsNotType<NotImplementedException>(ex);
+    }
+
+    [Fact]
+    public async void RecordAsync()
+    {
+        Func<Task> testCode = () => Task.Factory.StartNew(ThrowingNotImplementedExceptionMethod);
+
+        var ex = await Record.ExceptionAsync(testCode);
+
+        Assert.IsType<NotImplementedException>(ex);
+    }
+
+    [Fact]
+    public async void RecordAsync2()
+    {
+        Func<Task> testCode = () => Task.Factory.StartNew(ThrowingExceptionMethod);
+
+        var ex = await Record.ExceptionAsync(testCode);
+
+        Assert.IsType<Exception>(ex);
+    }
+
+    void ThrowingNotImplementedExceptionMethod()
+    {
+        throw new NotImplementedException();
+    }
+
+    void ThrowingExceptionMethod()
+    {
+        throw new Exception();
+    }
+}
+
